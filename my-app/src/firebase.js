272018db919e62921fb6f5da5b13f07eed0b3db2
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-11 15:45:08 -0400

    app created

---
+++ my-app/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCxFSG-wFr7d9dmRkZ7lbGs32ZBpqdBGL4",
+    authDomain: "covid-19-tracker-54dee.firebaseapp.com",
+    projectId: "covid-19-tracker-54dee",
+    storageBucket: "covid-19-tracker-54dee.appspot.com",
+    messagingSenderId: "674384105047",
+    appId: "1:674384105047:web:a5099d62c140cf7ffbdd12",
+    measurementId: "G-E7WG3G35KD"
+  };
