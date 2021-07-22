6e66d64167ba1dfd1ff989dc4786397e84711268
Sebastian <sebastian@SEBASTIAN-01.local>
2021-07-22 19:20:02 -0400

    app deployed

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCxFSG-wFr7d9dmRkZ7lbGs32ZBpqdBGL4",
     authDomain: "covid-19-tracker-54dee.firebaseapp.com",
